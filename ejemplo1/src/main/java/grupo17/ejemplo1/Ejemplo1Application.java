0ac5f53347e04ea596f8c966e5d6a1e45273e025
LUZ MARINA ORJUELA <programacion.luz.rojas@gmail.com>
2021-10-11 17:35:28 -0500

    Primera version para compilar en mv

---
+++ ejemplo1/src/main/java/grupo17/ejemplo1/Ejemplo1Application.java
@@ -0,0 +1,13 @@
+package grupo17.ejemplo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejemplo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejemplo1Application.class, args);
+	}
+
+}
